2ec5df3b2c456b6da435f943fb118107dc32a482
Shikha <shikhakumarisingh4@gmail.com>
2023-11-11 19:46:57 +0530

    Todo App

--- src/components/PopUp.jsx
+++ src/components/PopUp.jsx
@@ -1,23 +1,21 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useCallback } from "react";
 function useOutsideAlerter(ref, fn) {
-  const newFn = () => useCallback(() => fn(), []);
-
   useEffect(() => {
     function handleClickOutside(event) {
       if (ref.current && !ref.current.contains(event.target)) {
-        newFn();
+        fn();
       }
     }
     document.addEventListener("mousedown", handleClickOutside);
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [ref, newFn]);
+  }, [ref, fn]);
 }
 
 export default function PopUp({ children, onClickOutside }) {
   const wrapperRef = useRef(null);
-  useOutsideAlerter(wrapperRef, onClickOutside);
+  useOutsideAlerter(wrapperRef,  useCallback(() => onClickOutside(), []));
 
   return <div ref={wrapperRef}>{children}</div>;
 }
